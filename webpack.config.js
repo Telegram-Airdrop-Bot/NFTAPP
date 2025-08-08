62b933d873005d273577c756bda0b00fc07e3520
moonbd0529 <mushfiqurbd1701@gmail.com>
2025-08-08 18:16:48 +0600

    Add webpack config and polyfills to fix crypto module error

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const webpack = require('webpack');
+
+module.exports = {
+  resolve: {
+    fallback: {
+      crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('stream-browserify'),
+      buffer: require.resolve('buffer'),
+      process: require.resolve('process/browser'),
+      util: require.resolve('util'),
+      assert: require.resolve('assert'),
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+      os: require.resolve('os-browserify/browser'),
+      url: require.resolve('url'),
+    },
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+      process: 'process/browser',
+    }),
+  ],
+}; 
